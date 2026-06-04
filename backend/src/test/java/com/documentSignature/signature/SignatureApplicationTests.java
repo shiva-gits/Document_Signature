620267e5afcc30ff270060f642334acb2814d763
Shiva <shivaramkrishna031@gmail.com>
2026-06-05 00:48:23 +0530

    Day 1 completed initialized everything required.

---
+++ backend/src/test/java/com/documentSignature/signature/SignatureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.documentSignature.signature;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SignatureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

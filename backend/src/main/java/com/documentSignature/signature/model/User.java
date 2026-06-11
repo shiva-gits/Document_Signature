23e8d42659c7e56b42629311a79ab7753c09ec5d
Shiva <shivaramkrishna031@gmail.com>
2026-06-11 18:22:35 +0530

    added User Entity, Repository, and password encoder configuration

---
+++ backend/src/main/java/com/documentSignature/signature/model/User.java
@@ -0,0 +1,37 @@
+package com.documentSignature.signature.model;
+
+// import org.hibernate.annotations.EmbeddableInstantiator;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+@Entity
+@Table(name = "users", uniqueConstraints = { @UniqueConstraint(columnNames = "email") })
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Size(max = 50)
+    private String name;
+
+    @NotBlank
+    @Size(max = 50)
+    @Email
+    private String email;
+
+    @NotBlank
+    @Size(max = 120)
+    private String password;
+
+}
